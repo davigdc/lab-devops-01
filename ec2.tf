80918ea08371a3577ce1a1986dd1a1c2d33f8ae5
davigdc <davigdc10@gmail.com>
2022-06-06 16:20:02 -0300

    exec docker

--- ec2.tf
+++ ec2.tf
@@ -1,13 +1,32 @@
 resource "aws_instance" "app_server" {
-  ami           = "ami-0ee8244746ec5d6d4"
-  instance_type = "t2.micro"
-  subnet_id = aws_subnet.sb_lab01.id
-  vpc_security_group_ids = [aws_security_group.sg_lab_access.id]
+  ami                         = "ami-0ee8244746ec5d6d4"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.sb_lab01.id
+  vpc_security_group_ids      = [aws_security_group.sg_lab_access.id]
   associate_public_ip_address = true
-  key_name = "lab-devops-01"
+  key_name                    = "lab-devops-01"
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file(var.pvt_key)
+    host        = self.public_ip
+  }
 
   provisioner "local-exec" {
     command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i '${self.public_ip},' -u ubuntu --private-key=${var.pvt_key} install-docker.yaml"
+  }
+
+  provisioner "file" {
+    source      = "app"
+    destination = "/tmp"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/app/script.sh",
+      "/tmp/app/script.sh"
+    ]
   }
 
   tags = {
