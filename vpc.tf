9400b6f180dc49954a3466edf51614137d0e9c25
davigdc <davigdc10@gmail.com>
2022-06-06 16:18:39 -0300

    security group change

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "vpc_lab01" {
-  cidr_block       = "10.0.0.0/24"
+  cidr_block           = "10.0.0.0/24"
   enable_dns_hostnames = true
 
   tags = {
@@ -8,8 +8,8 @@ resource "aws_vpc" "vpc_lab01" {
 }
 
 resource "aws_subnet" "sb_lab01" {
-  vpc_id     = aws_vpc.vpc_lab01.id
-  cidr_block = "10.0.0.0/24"
+  vpc_id            = aws_vpc.vpc_lab01.id
+  cidr_block        = "10.0.0.0/24"
   availability_zone = "us-west-2a"
 
   tags = {
@@ -26,21 +26,21 @@ resource "aws_internet_gateway" "ig_lab01" {
 }
 
 resource "aws_route_table" "rt_lab01" {
-    vpc_id = aws_vpc.vpc_lab01.id
+  vpc_id = aws_vpc.vpc_lab01.id
 
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.ig_lab01.id
-    }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.ig_lab01.id
+  }
 
-    tags = {
-        Name = var.tag_name
-    }
+  tags = {
+    Name = var.tag_name
+  }
 }
 
 resource "aws_route_table_association" "art_lab01" {
-    subnet_id = aws_subnet.sb_lab01.id
-    route_table_id = aws_route_table.rt_lab01.id
+  subnet_id      = aws_subnet.sb_lab01.id
+  route_table_id = aws_route_table.rt_lab01.id
 }
 
 resource "aws_security_group" "sg_lab_access" {
@@ -60,14 +60,6 @@ resource "aws_security_group" "sg_lab_access" {
     description = "HTTP to EC2"
     from_port   = 80
     to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "HTTPS to EC2"
-    from_port   = 443
-    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
